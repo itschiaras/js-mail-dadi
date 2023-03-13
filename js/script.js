810952175c638ff499c5e9bd321b3cc0d3e66624
ChiaraS97 <chiara.sucato97@gmail.com>
2023-03-13 13:19:12 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Mail
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+const allowedEmails = [
+"kaylahrussel@konopelski.com",
+"pdach@gmail.com",
+"mkoelpin@bernier.com",
+"ivy.sipes@hotmail.com",
+"jhickle@yahoo.com",
+"mann.mallory@grimes.com",
+"morissette.milan@reilly.net",
+"hmarquardt@yahoo.com",
+"sierra.schimmel@johns.com",
+"karelle.runolfsdottir@stokes.com"
+]
